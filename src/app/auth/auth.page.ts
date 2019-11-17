8a22d227dfaa98eaf627f113bf518560d9d16a57
RoamingAroundRome <44724366+RoamingAroundRome@users.noreply.github.com>
2019-11-17 19:27:24 +0800

    Edited offers page and auth (component # 11)

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 
 import { AuthService } from './auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -11,6 +12,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -33,5 +35,23 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
+  onSwitchAuthMode(){
+    this.isLogin = !this.isLogin;
+  }
+  onSubmit(form:NgForm){
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email,password);
+    
+    if(this.isLogin){
+      //Send Login r
+    }
+    else{
+
+    }
   }
 }
--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-new-offer',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.form = new FormGroup({
+      title : new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl (null,{
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(1)]
+      }),
+      dateFrom: new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null,{
+        validators: [Validators.required]
+      })
+    });
+  } 
 
+  onCreateOffer(){
+    console.log(this.form);
+  }
 }
